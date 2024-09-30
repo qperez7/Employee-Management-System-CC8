ae1e457a02d6457fd1de0142e5399c7aed9a6f9e
Quirianyailet Perez <qperez1@usf.edu>
2024-09-30 16:30:25 -0400

    Create Employee class with properties and methods

---
+++ employee_management.js
@@ -0,0 +1,10 @@
+// Task 1: Create an Employee Class
+class Employee {
+    constructor(name,salary,position,department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+}}
+getDetails()
+return `${this.name},${this.position},${this.salary},${this.department}`
