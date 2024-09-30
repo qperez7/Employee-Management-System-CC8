48c834131c611af6877b2c3ccf6fcddff779e58a
Quirianyailet Perez <qperez1@usf.edu>
2024-09-30 17:10:50 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -5,6 +5,26 @@ class Employee {
         this.salary = salary;
         this.position = position;
         this.department = department;
-}}
-getDetails()
+}
+getDetails() {
 return `${this.name},${this.position},${this.salary},${this.department}`
+}}
+console.log(Employee.getDetails);
+// Task 2: Create a Department Class
+class Department {
+    constructor (name,[]) {
+        this.name = name;
+        this.employees = [];
+    }
+addEmployee(employee) {
+    return this.employees.push(employee)
+}
+getDepartmentSalary() {
+    return this.employees.reduce((acc,salary) =>  acc + employee.salary,0)
+    
+}
+}
+console.log(Department.addEmployee);
+console.log(Department.getDepartmentSalary);
+// Task 3: Create a Manager Class that Inherits from Employee
+
